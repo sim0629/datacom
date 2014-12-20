7c3670c901994e2dee40bf0ec89a16512b84ce66
sgm <sim0629@gmail.com>
2014-12-20 17:02:59 +0900

    Implement Simulator

--- Datacom/Simulator.cs
+++ Datacom/Simulator.cs
@@ -6,8 +6,45 @@ using System.Threading.Tasks;
 
 namespace Gyumin.Datacom
 {
-    class Simulator
+    using Models;
+
+    public class Simulator
     {
+        public enum NodeType { Backoff, Dcf }
+
+        private Medium medium;
+        private List<Node> nodes;
+
+        public Simulator(int n, int lambda, NodeType type)
+        {
+            medium = new Medium();
+            nodes = new List<Node>(n);
+            for (var i = 0; i < n; i++) {
+                Node node = null;
+                switch (type)
+                {
+                    case NodeType.Backoff:
+                        node = new BackoffNode(medium, lambda);
+                        break;
+                    case NodeType.Dcf:
+                        node = new DcfNode(medium, lambda);
+                        break;
+                    default:
+                        throw new ArgumentException("Unknown NodeType", "type");
+                }
+                nodes.Add(node);
+            }
+        }
+
+        public void Run()
+        {
+            for (var i = 0; i < Constants.SIMUL_DURATION_TIME; i++)
+            {
+                nodes.ForEach(node => node.Next(i));
+                medium.Next();
+            }
+        }
+
         static void Main(string[] args)
         {
         }
